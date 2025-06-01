07dc33cba33984321dd840008983df205e240dbd
Hao Hoang <haohoangofficial@gmail.com>
2025-06-01 17:57:47 +0700

    Update

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,5 +1,9 @@
 import re, os
 from utils.fairautoml_tuners_utils import load_hyperparameter_space
+
+N_RS_TRIALS = 100
+
+
 configs_path = '/kaggle/working/assets/hyperparameters/hyperparameter-spaces/ACSIncome-Adult'
 yaml_regex = re.compile(r"^(?P<name>.+)[\.]hyperparameter-space.yaml$")
 hyperparam_spaces = dict()
@@ -12,3 +16,21 @@ for file_name in os.listdir(configs_path):
         file_path = os.path.join(configs_path, file_name)
         hyperparam_spaces[m.group("name")] = load_hyperparameter_space(file_path)
         print(hyperparam_spaces)
+
+from utils.hyperparams import suggest_random_hyperparams_with_classpath
+
+configs = {
+    algo_name: [
+        suggest_random_hyperparams_with_classpath(hyper_space, seed=rng.randrange(2**32 - 1))
+        for _ in range(N_RS_TRIALS)
+    ]
+    for algo_name, hyper_space in hyperparam_spaces.items()
+}
+
+# Save configs to disk
+import json
+
+configs_file_path = os.path.join(configs_path, f"configs.{N_RS_TRIALS}-trials-per-algo.json")
+with open(configs_file_path, "w") as out_file:
+    json.dump(configs, out_file, indent=4)
+    print(f"Saved configs to JSON file at '{configs_file_path}'")
