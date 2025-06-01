79f336a7d41578954c4cc3659ccad65994699b33
Hao Hoang <haohoangofficial@gmail.com>
2025-06-01 17:52:22 +0700

    Update

--- notebooks/main.py
+++ notebooks/main.py
@@ -10,3 +10,4 @@ for file_name in os.listdir(configs_path):
     if m:
         file_path = configs_path / file_name
         hyperparam_spaces[m.group("name")] = load_hyperparameter_space(file_path)
+        print(hyperparam_spaces)
