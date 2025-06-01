d98aa859d972bd6677b05ab3bb312258b9a2e9c3
Hao Hoang <haohoangofficial@gmail.com>
2025-06-01 16:27:24 +0700

    (feat): add source

---
+++ notebooks/main.py
@@ -0,0 +1,12 @@
+import re, os
+from utils.fairautoml_tuners_utils import load_hyperparameter_space
+configs_path = 
+yaml_regex = re.compile(r"^(?P<name>.+)[\.]hyperparameter-space.yaml$")
+hyperparam_spaces = dict()
+
+for file_name in os.listdir(configs_path):
+    m = yaml_regex.match(file_name)
+
+    if m:
+        file_path = configs_path / file_name
+        hyperparam_spaces[m.group("name")] = load_hyperparameter_space(file_path)
